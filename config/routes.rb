9536fd2d2c41c48b6d56cacabb344e23bf43d46e
ArutuR2 <shinyuuhou@gmail.com>
2023-07-28 09:33:21 +0900

    [test] change q2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "home" => "home#top"
   get "home/enterName" => "home#enterName"
   get "home/q1/:nickname" => "home#q1"
-  get "home/q2" => "home#q2"
+  get "home/q2/:nickname" => "home#q2"
   get "home/log" => "home#log"
   post "home/confirm/:nickname" => "home#confirm"
   post "home/reset" => "home#reset"
